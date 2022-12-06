815c0fcbdae4c2ceac105bb88587221311f0697a
Gasol Wu <gasol.wu@gmail.com>
2022-12-06 11:08:00 +0800

    Solve the final part of Day 1

--- src/main/kotlin/tw/gasol/aoc/aoc2022/Day1.kt
+++ src/main/kotlin/tw/gasol/aoc/aoc2022/Day1.kt
@@ -1,20 +1,29 @@
 package tw.gasol.aoc.aoc2022
 
 class Day1 {
-    fun part1(input: String): Int {
-        val caloriesGroups = input.splitToSequence("\n\n")
+
+    private fun getCaloriesGroups(input: String): Sequence<Sequence<Int>> {
+        return input.splitToSequence("\n\n")
             .map { chunk ->
                 chunk.splitToSequence("\n")
                     .filterNot { it.isBlank() }
                     .map { it.toInt() }
-                    .sum()
             }
-        return caloriesGroups
+    }
+
+    private fun getCaloriesSums(input: String) =
+        getCaloriesGroups(input).map { it.sum() }
+
+    fun part1(input: String): Int {
+        return getCaloriesSums(input)
             .sortedDescending()
             .first()
     }
 
     fun part2(input: String): Int {
-        return 0
+        return getCaloriesSums(input)
+            .sortedDescending()
+            .take(3)
+            .sum()
     }
 }
--- src/test/kotlin/tw/gasol/aoc/aoc2022/Day1Test.kt
+++ src/test/kotlin/tw/gasol/aoc/aoc2022/Day1Test.kt
@@ -5,10 +5,20 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions.*
 
 internal class Day1Test {
+    fun readInput(): String {
+        return javaClass.getResourceAsStream("/input/day1.txt")
+            ?.bufferedReader().use { it!!.readText() }
+    }
+
     @Test
     fun part1() {
-        val inputText = javaClass.getResourceAsStream("/input/day1.txt").bufferedReader().use { it.readText() }
-        val result = Day1().part1(inputText)
+        val result = Day1().part1(readInput())
         assertEquals(69289, result)
+    }
+
+    @Test
+    fun part2() {
+        val result = Day1().part2(readInput())
+        assertEquals(205615, result)
     }
 }
