8508d9cc8ecc0181d028feca32566f21c59ca0ac
Gasol Wu <gasol.wu@gmail.com>
2022-12-07 20:20:39 +0800

    Refactor by using sliding windows
    Get the idea from https://blog.jetbrains.com/kotlin/2021/12/tips-and-tricks-for-solving-advent-of-code/#8_Sliding_by_windows

--- src/main/kotlin/tw/gasol/aoc/aoc2022/Day6.kt
+++ src/main/kotlin/tw/gasol/aoc/aoc2022/Day6.kt
@@ -2,26 +2,12 @@ package tw.gasol.aoc.aoc2022
 
 class Day6 {
 
-    private fun getDistinctSignalPosition(input: String, signalSize: Int = 4): IntRange? {
-        for (i in 0..input.length) {
-            val endPosition = i + signalSize
-            if (endPosition > input.length) {
-                break
-            }
-            val s = input.substring(i, endPosition)
-            if (s.toSet().size == signalSize) {
-                return i .. endPosition
-            }
-        }
+    private fun getDistinctSignalPosition(input: String, signalSize: Int = 4): Int =
+        input.toList()
+            .windowed(signalSize)
+            .indexOfFirst { it.distinct().size == signalSize } + signalSize
 
-        return null
-    }
+    fun part1(input: String) = getDistinctSignalPosition(input, 4)
 
-    fun part1(input: String): Int {
-        return getDistinctSignalPosition(input, 4)?.endInclusive ?: 0
-    }
-
-    fun part2(input: String): Int {
-        return getDistinctSignalPosition(input, 14)?.endInclusive ?: 0
-    }
+    fun part2(input: String) = getDistinctSignalPosition(input, 14)
 }
