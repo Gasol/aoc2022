7481f2e737b8402e4af168b145e0372a22c6a615
Gasol Wu <gasol.wu@gmail.com>
2022-12-07 11:19:33 +0800

    Solve the part 1 of Day 5

---
+++ src/main/kotlin/tw/gasol/aoc/aoc2022/Day5.kt
@@ -0,0 +1,86 @@
+package tw.gasol.aoc.aoc2022
+
+import java.util.Scanner
+
+class Day5 {
+
+    fun part1(input: String): String {
+        val stackBuilder = StringBuilder()
+        var stacks: List<ArrayDeque<Char>>? = null
+        input.lineSequence()
+            .forEach { line ->
+                if (stacks == null) {
+                    if (line.isBlank()) {
+                        stacks = buildStack(stackBuilder.toString())
+                    } else {
+                        stackBuilder.appendLine(line)
+                    }
+                } else if (line.isNotBlank()) {
+                    val procedure = parseProcedure(line)
+                    runProcedure(stacks!!, procedure)
+                }
+            }
+
+        return stacks?.map { it.last() }?.joinToString("").orEmpty()
+    }
+
+    private fun runProcedure(stacks: List<ArrayDeque<Char>>, procedure: MoveProcedure) {
+        repeat(procedure.quantity) {
+            val from = stacks[procedure.from - 1]
+            val to = stacks[procedure.to - 1]
+            to.addLast(from.removeLast())
+        }
+    }
+
+    private fun parseProcedure(line: String) = MoveProcedure.fromString(line)
+
+    private fun buildStack(input: String): List<ArrayDeque<Char>> {
+        val crateLines = input.lines()
+            .filterNot { it.isBlank() }
+            .filterNot { it.startsWith(" 1 ") }
+
+        val emptyCrateReplacement = "[-]"
+        val crateLists = crateLines
+            .map { line ->
+                line.replace("^ {3}".toRegex(), emptyCrateReplacement)
+                    .replace("    ", " $emptyCrateReplacement")
+                    .split(" ")
+                    .filterNot { it.isBlank() }
+                    .map { it.trim()[1] }
+            }
+            .filterNot { it.isEmpty() }
+
+        val stackSize = crateLists.maxOf { it.size }
+        val stacks = buildList(stackSize) {
+            repeat(stackSize) { add(ArrayDeque<Char>()) }
+        }
+
+        crateLists.flatten()
+            .forEachIndexed { index, crate ->
+                if (crate != '-') {
+                    stacks[index % stacks.size].addFirst(crate)
+                }
+            }
+        return stacks
+    }
+
+    fun part2(input: String): Int {
+        return 0
+    }
+}
+
+data class MoveProcedure(val quantity: Int, val from: Int, val to: Int) {
+    companion object {
+        fun fromString(line: String): MoveProcedure = Scanner(line).use { scanner ->
+            val command = scanner.next()
+            assert(command == "move") { "Invalid command: $command" }
+            val quantity = scanner.nextInt()
+            assert(quantity >= 0) { "Invalid quantity: $quantity" }
+            scanner.next()
+            val from = scanner.nextInt()
+            scanner.next()
+            val to = scanner.nextInt()
+            return MoveProcedure(quantity, from, to)
+        }
+    }
+}
---
+++ src/test/kotlin/tw/gasol/aoc/aoc2022/Day5Test.kt
@@ -0,0 +1,20 @@
+package tw.gasol.aoc.aoc2022
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class Day5Test {
+
+    private val input = readInput("/input/day5.txt")
+
+    @Test
+    fun part1() {
+        val result = Day5().part1(input)
+        assertEquals("GFTNRBZPF", result)
+    }
+
+    @Test
+    fun part2() {
+    }
+}
