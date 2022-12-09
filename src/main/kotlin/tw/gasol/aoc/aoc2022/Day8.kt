1d13420b7b68d7c28e19ee480fe8feffa2b47012
Gasol Wu <gasol.wu@gmail.com>
2022-12-09 10:24:13 +0800

    Count visible lazily

--- src/main/kotlin/tw/gasol/aoc/aoc2022/Day8.kt
+++ src/main/kotlin/tw/gasol/aoc/aoc2022/Day8.kt
@@ -15,16 +15,36 @@ class Day8 {
     }
 }
 
-
 class TreeMap(private val width: Int, private val height: Int, private val heights: List<Char>) {
 
     private val treeMap = CharArray(width * height) { index ->
         heights[index]
     }
 
-    private val visibleMap = BooleanArray(width * height) { false }
+    fun get(x: Int, y: Int): Char {
+        assert(x in 0 until width) { "x is out of range" }
+        assert(y in 0 until height) { "y is out of range" }
+        return treeMap[y * width + x]
+    }
 
-    init {
+    private fun getHeightList(x: Int, y: Int, from: Direction): List<Char> {
+        return when (from) {
+            Direction.TOP -> (0 until height).map { get(x, it) }
+            Direction.BOTTOM -> (height - 1 downTo 0).map { get(x, it) }
+            Direction.LEFT -> (0 until width).map { get(it, y) }
+            Direction.RIGHT -> (width - 1 downTo 0).map { get(it, y) }
+        }
+    }
+
+    private fun buildVisibleMap(): BooleanArray {
+        val visibleMap = BooleanArray(width * height) { false }
+
+        val setVisible = { x: Int, y: Int, value: Boolean ->
+            assert(x in 0 until width) { "x ($x) is out of range" }
+            assert(y in 0 until height) { "y ($y) is out of range" }
+            visibleMap[y * width + x] = value
+        }
+
         (0 until width).forEach { x ->
             var baseHeight = '.'
             getHeightList(x, 0, Direction.TOP).forEachIndexed { y, height ->
@@ -74,29 +94,22 @@ class TreeMap(private val width: Int, private val height: Int, private val heigh
                 }
             }
         }
+        return visibleMap
     }
 
-    fun get(x: Int, y: Int): Char {
-        assert(x in 0 until width) { "x is out of range" }
-        assert(y in 0 until height) { "y is out of range" }
-        return treeMap[y * width + x]
-    }
-
-    private fun getHeightList(x: Int, y: Int, from: Direction): List<Char> {
-        return when (from) {
-            Direction.TOP -> (0 until height).map { get(x, it) }
-            Direction.BOTTOM -> (height - 1 downTo 0).map { get(x, it) }
-            Direction.LEFT -> (0 until width).map { get(it, y) }
-            Direction.RIGHT -> (width - 1 downTo 0).map { get(it, y) }
-        }
-    }
-
-    fun countVisible() = visibleMap.count { it }
+    fun countVisible() = buildVisibleMap().count { it }
 
     fun printVisibleMap() {
+        val visibleMap = buildVisibleMap()
+        val getVisible = { x: Int, y: Int ->
+            assert(x in 0 until width) { "x ($x) is out of range" }
+            assert(y in 0 until height) { "y ($y) is out of range" }
+            visibleMap[y * width + x]
+        }
+
         (0 until height).forEach { y ->
             (0 until width).forEach { x ->
-                print(if (getVisible(x, y)) "1" else "0")
+                print(if (getVisible(x, y)) 'V' else ' ')
             }
             println()
         }
@@ -153,18 +166,6 @@ class TreeMap(private val width: Int, private val height: Int, private val heigh
         return arrayOf(::computeUpScore, ::computeLeftScore, ::computeRightScore, ::computeDownScore)
             .map { it(x, y, treeHeight) }
             .reduce(Math::multiplyExact)
-    }
-
-    private fun setVisible(x: Int, y: Int, value: Boolean) {
-        assert(x in 0 until width) { "x ($x) is out of range" }
-        assert(y in 0 until height) { "y ($y) is out of range" }
-        visibleMap[y * width + x] = value
-    }
-
-    private fun getVisible(x: Int, y: Int): Boolean {
-        assert(x in 0 until width) { "x ($x) is out of range" }
-        assert(y in 0 until height) { "y ($y) is out of range" }
-        return visibleMap[y * width + x]
     }
 
     companion object {
