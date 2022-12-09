70b2fd2ac51ab6206d27bc0a401625302c0f751b
Gasol Wu <gasol.wu@gmail.com>
2022-12-09 15:31:59 +0800

    Solve the frst part of Day 9

--- src/main/kotlin/tw/gasol/aoc/aoc2022/Day9.kt
+++ src/main/kotlin/tw/gasol/aoc/aoc2022/Day9.kt
@@ -1,9 +1,60 @@
 package tw.gasol.aoc.aoc2022
 
+typealias Position = Pair<Int, Int>
+
 class Day9 {
-    fun part1(input: String): Int {
-        return 0
+    enum class Direction {
+        UP, DOWN, LEFT, RIGHT;
+
+        companion object {
+            fun fromString(str: String): Direction {
+                return when (str) {
+                    "U" -> UP
+                    "D" -> DOWN
+                    "L" -> LEFT
+                    "R" -> RIGHT
+                    else -> throw IllegalArgumentException("Unknown direction: $str")
+                }
+            }
+        }
     }
+
+    fun part1(input: String): Int {
+        val regex = "(\\w) (\\d+)".toRegex()
+        val movements = input.lines()
+            .filterNot { it.isBlank() }
+            .map { line ->
+                val (direction, distance) = regex.matchEntire(line)
+                    ?.destructured ?: error("Invalid input line: $line")
+                Pair(Direction.fromString(direction), distance.toInt())
+            }
+        val locations = mutableListOf<Position>()
+        locations.add(0 to 0) // s
+        var previousDirection: Direction? = null
+        movements.forEach { movement ->
+            val (direction, distance) = movement
+            val last = locations.last()
+            val (lastX, lastY) = last
+            val (offsetX, offsetY) = when (direction) {
+                Direction.UP -> 0 to 1
+                Direction.DOWN -> 0 to -1
+                Direction.LEFT -> -1 to 0
+                Direction.RIGHT -> 1 to 0
+            }
+            val movedLocations =
+                (1..distance).map {
+                    Position(lastX + (offsetX * it), lastY + (offsetY * it))
+                }
+            if (previousDirection != null && previousDirection != direction) {
+                locations.removeLast()
+            }
+            locations.addAll(movedLocations)
+            previousDirection = direction
+        }
+
+        return locations.toSet().size - 1
+    }
+
     fun part2(input: String): Int {
         return 0
     }
--- src/test/kotlin/tw/gasol/aoc/aoc2022/Day9Test.kt
+++ src/test/kotlin/tw/gasol/aoc/aoc2022/Day9Test.kt
@@ -3,18 +3,37 @@ package tw.gasol.aoc.aoc2022
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
+import kotlin.test.Ignore
 
 class Day9Test {
 
     private val input = readInput("/input/day9.txt")
 
+    private val sampleInput = """
+        R 4
+        U 4
+        L 3
+        D 1
+        R 4
+        D 1
+        L 5
+        R 2
+    """.trimIndent()
+
     @Test
     fun part1() {
         val result = Day9().part1(input)
-        assertEquals(0, result);
+        assertEquals(6057, result);
     }
 
     @Test
+    @Ignore
     fun part2() {
+    }
+
+    @Test
+    fun samplePart1() {
+        val result = Day9().part1(sampleInput)
+        assertEquals(13, result)
     }
 }
