22d612045c6f11c5a7544063b06fc10716271d48
Gasol Wu <gasol.wu@gmail.com>
2022-12-09 14:07:03 +0800

    Init Day9

---
+++ src/main/kotlin/tw/gasol/aoc/aoc2022/Day9.kt
@@ -0,0 +1,10 @@
+package tw.gasol.aoc.aoc2022
+
+class Day9 {
+    fun part1(input: String): Int {
+        return 0
+    }
+    fun part2(input: String): Int {
+        return 0
+    }
+}
---
+++ src/test/kotlin/tw/gasol/aoc/aoc2022/Day9Test.kt
@@ -0,0 +1,16 @@
+package tw.gasol.aoc.aoc2022
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class Day9Test {
+
+    @Test
+    fun part1() {
+    }
+
+    @Test
+    fun part2() {
+    }
+}
