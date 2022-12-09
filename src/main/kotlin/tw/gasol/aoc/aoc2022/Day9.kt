69e11c66594cd5617ae7ec6f84a6fddb06f41e77
Gasol Wu <gasol.wu@gmail.com>
2022-12-09 18:56:05 +0800

    Complete Day 9

--- src/main/kotlin/tw/gasol/aoc/aoc2022/Day9.kt
+++ src/main/kotlin/tw/gasol/aoc/aoc2022/Day9.kt
@@ -1,5 +1,7 @@
 package tw.gasol.aoc.aoc2022
 
+import kotlin.math.absoluteValue
+
 typealias Position = Pair<Int, Int>
 
 class Day9 {
@@ -20,18 +22,11 @@ class Day9 {
     }
 
     fun part1(input: String): Int {
-        val regex = "(\\w) (\\d+)".toRegex()
-        val movements = input.lines()
-            .filterNot { it.isBlank() }
-            .map { line ->
-                val (direction, distance) = regex.matchEntire(line)
-                    ?.destructured ?: error("Invalid input line: $line")
-                Pair(Direction.fromString(direction), distance.toInt())
-            }
+
         val locations = mutableListOf<Position>()
         locations.add(0 to 0) // s
         var previousDirection: Direction? = null
-        movements.forEach { movement ->
+        readMovements(input).forEach { movement ->
             val (direction, distance) = movement
             val last = locations.last()
             val (lastX, lastY) = last
@@ -55,7 +50,100 @@ class Day9 {
         return locations.toSet().size - 1
     }
 
+    private fun readMovements(input: String): List<Pair<Direction, Int>> {
+        val regex = "(\\w) (\\d+)".toRegex()
+        return input.lines()
+            .filterNot { it.isBlank() }
+            .map { line ->
+                val (direction, distance) = regex.matchEntire(line)
+                    ?.destructured ?: error("Invalid input line: $line")
+                Pair(Direction.fromString(direction), distance.toInt())
+            }
+    }
+
     fun part2(input: String): Int {
-        return 0
+        val movements = readMovements(input)
+        val rope = Rope(10)
+        return rope.countTailVisited(movements)
+    }
+
+    class Rope(private val numKnots: Int) {
+        fun countTailVisited(movements: List<Pair<Direction, Int>>): Int {
+            val visited = mutableSetOf<Position>()
+            for (movement in movements) {
+                move(movement) {
+                    val tail = knots.last()
+                    visited.add(Position(tail.x, tail.y))
+                }
+            }
+            return visited.count()
+        }
+
+        private fun move(movement: Pair<Direction, Int>, onMoved: () -> Unit = {}) {
+            val (direction, distance) = movement
+            repeat(distance) {
+                knots[0].move(direction)
+                for (i in 1 until knots.size) {
+                    knots[i].follow(knots[i - 1])
+                }
+                onMoved()
+            }
+        }
+
+        data class Knot(var initX: Int, var initY: Int) {
+            var x = initX
+                private set
+            var y = initY
+                private set
+
+            fun move(direction: Direction) {
+                val (offsetX, offsetY) = when (direction) {
+                    Direction.UP -> 0 to 1
+                    Direction.DOWN -> 0 to -1
+                    Direction.LEFT -> -1 to 0
+                    Direction.RIGHT -> 1 to 0
+                }
+
+                x += offsetX
+                y += offsetY
+            }
+
+            fun follow(front: Knot) {
+                val diffX = front.x - x
+                val diffY = front.y - y
+                if (diffX.absoluteValue < 2 && diffY.absoluteValue < 2) {
+                    return
+                }
+
+                if (diffX == 0) {
+                    y += diffY / 2
+                } else if (diffX == 1) {
+                    x += 1
+                    y += diffY / 2
+                } else if (diffX == -1) {
+                    x -= 1
+                    y += diffY / 2
+                } else if (diffY == 1) {
+                    x += diffX / 2
+                    y += 1
+                } else if (diffY == 0) {
+                    x += diffX / 2
+                } else if (diffY == -1) {
+                    x += diffX / 2
+                    y -= 1
+                } else {
+                    x += diffX / 2
+                    y += diffY / 2
+                }
+            }
+
+            override fun toString(): String {
+                return "Knot($x, $y)"
+            }
+        }
+
+        private val knots = buildList {
+            repeat(numKnots) { add(Knot(0, 0)) }
+        }
     }
 }
--- src/test/kotlin/tw/gasol/aoc/aoc2022/Day9Test.kt
+++ src/test/kotlin/tw/gasol/aoc/aoc2022/Day9Test.kt
@@ -1,9 +1,7 @@
 package tw.gasol.aoc.aoc2022
 
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-
-import org.junit.jupiter.api.Assertions.*
-import kotlin.test.Ignore
 
 class Day9Test {
 
@@ -27,13 +25,56 @@ class Day9Test {
     }
 
     @Test
-    @Ignore
     fun part2() {
+        val result = Day9().part2(input)
+        assertEquals(2514, result);
     }
 
     @Test
     fun samplePart1() {
         val result = Day9().part1(sampleInput)
         assertEquals(13, result)
+    }
+
+    @Test
+    fun samplePart2() {
+        val result = Day9().part2(sampleInput)
+        assertEquals(1, result)
+    }
+
+    @Test
+    fun testFollowWithNoOp() {
+        val head = Day9.Rope.Knot(0, 0)
+        for (x in -1..1) {
+            for (y in -1..1) {
+                val tail = Day9.Rope.Knot(x, y)
+                tail.follow(head)
+                assertEquals(x, tail.x)
+                assertEquals(y, tail.y)
+            }
+        }
+    }
+
+    @Test
+    fun testFollowByMoveOneStepDiagonally() {
+        val head = Day9.Rope.Knot(0, 0)
+
+        val test = listOf(
+            Pair(-1, -2) to Pair(0, -1),
+            Pair(1, -2) to Pair(0, -1),
+            Pair(2, -2) to Pair(1, -1),
+            Pair(2, -1) to Pair(1, 0),
+            Pair(2, 0) to Pair(1, 0),
+            Pair(2, 1) to Pair(1, 0),
+            Pair(2, 2) to Pair(1, 1),
+            Pair(0, 2) to Pair(0, 1),
+            Pair(0, -2) to Pair(0, -1),
+        )
+        for ((input, expected) in test) {
+            val (x, y) = input
+            val tail = Day9.Rope.Knot(x, y)
+            tail.follow(head)
+            assertEquals(expected, tail.x to tail.y)
+        }
     }
 }
